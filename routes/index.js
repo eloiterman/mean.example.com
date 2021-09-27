898d9a1eaf0b371292ec8c132ad472378c860348
chad svastisalee <swasgrafx@gmail.com>
2021-09-27 11:28:15 -0500

    Add .editorconfig

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', name: 'MicroTrain2108'});
 });
 
 module.exports = router;

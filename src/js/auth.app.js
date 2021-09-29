eaf211ace63bf58db974e8ef07e60141f734557b
chad svastisalee <swasgrafx@gmail.com>
2021-09-29 10:15:15 -0500

    Add custom HTML5 validation

--- src/js/auth.app.js
+++ src/js/auth.app.js
@@ -122,9 +122,39 @@ var authApp = (function() {
       load: function(){
         registrationForm();
         postRequest('registrationForm', '/api/auth/register');
+        // Add Validation
+        validate.registrationForm();
       }
     }
   
   })();
-  
+
+  // ~line 121
+var validate = (function() {
+
+  function confirmPasswordMatch() {
+
+    let pw = document.getElementById('password');
+    let cpw = document.getElementById('confirm_password');
+
+    if(pw.value !== cpw.value){
+      cpw.setCustomValidity("Passwords do not match");
+    } else {
+      cpw.setCustomValidity("");
+    }
+
+  }
+
+  return {
+    registrationForm: function(){
+      document.querySelector('#registrationForm input[type="submit"]').addEventListener(
+        'click',
+        function(){
+        confirmPasswordMatch();
+      });
+    }
+  }
+
+})();
+
   authApp.load();

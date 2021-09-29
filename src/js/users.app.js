1123dcfdba111ae52d1713d9ab0a351c526f26a3
chad svastisalee <swasgrafx@gmail.com>
2021-09-29 12:08:11 -0500

    Build a View for Deleting a User

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -206,7 +206,10 @@ xhr.onload = function(){
                 <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
               </div>
             </div>
-  
+            <div>
+            <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
+            </div>
+          
             <div class="text-right">
               <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
             </div>
@@ -255,6 +258,52 @@ xhr.onload = function(){
           }
         });
       }
+
+//~line 250
+function deleteView(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+  
+      card = `<div class="card bg-transparent border-danger text-danger bg-danger">
+        <div class="card-header bg-transparent border-danger">
+          <h2 class="h3 text-center">Your About to Delete a User</h2>
+        </div>
+        <div class="card-body text-center">
+          <div>
+            Are you sure you want to delete
+            <strong>${data.user.first_name} ${data.user.last_name}</strong>
+          </div>
+  
+          <div>Username: <strong>${data.user.username}</strong></div>
+          <div>Email: <strong>${data.user.email}</strong></div>
+  
+          <div class="text-center">
+            <br>
+            <a class="btn btn-lg btn-danger text-white">
+              Yes delete ${data.user.username}
+            </a>
+          </div>
+  
+        </div>
+      </div>`;
+  
+      app.innerHTML = card;
+    }
+  }
         
     return {
     //Basic Navigation Structure 
@@ -277,9 +326,9 @@ xhr.onload = function(){
             break;
                   
         case '#delete':
-            console.log('DELETE');
+            deleteView(hashArray[1]);
             break;
-    
+                  
         default:
             viewUsers();
             break;
